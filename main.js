37efb355fcd4b567e1609c5c15f8298bf20cbfc6
MDZ <104726280+mdesignfa@users.noreply.github.com>
2024-01-14 16:55:32 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+function mdz_isdom_in_viewport(el, callback = '') {
 
+
+    var options = {
+        root: hex_confs.viewed_elems_root,
+        rootMargin: hex_confs.viewed_elems_rootmarg,
+        threshold: hex_confs.viewed_elems_threshold,
+    };
+
+
+    let observer = new IntersectionObserver((entry, observer) => {
+
+        if (entry[0].isIntersecting) {
+            entry[0].target.classList.add("is-in-user-viewport");
+
+            if (callback != '') {
+                callback();
+            }
+
+        }
+
+
+    }, options);
+
+    observer.observe(el);
+
+}
